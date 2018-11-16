8f963a1f959e2adf8b9d4769fc2db537fb5876eb
Spencer West <spencer.west.zbt@gmail.com>
2018-11-16 12:33:07 -0800

    updates

--- content_script.js
+++ content_script.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
   var interval = setInterval(doStuff, 2000); // 2000 ms = start after 2sec
   function doStuff() {
     var sorting1 = $('.sorting_1');
-    if (sorting1.length > 0) {
-      alert('hi');
-      var items = $('.sorting_1[data-title]');
+    var items = $('.sorting_1[data-title]');
+    if (sorting1.length > 0 && items.length > 0) {
       for(var i=0; i<items.length; i++) {
         var amazonTitle = items[i].getAttribute('data-title');
-        items[i].append('<a href=https://www.amazon.com/s/?field-keywords='+amazonTitle+'>Amazon</a>');
+        items[i].innerHTML += '<a class="redClass" target="_blank" href="https://www.google.com/search?q='+amazonTitle+'">Amazon</a>';
       }
       clearInterval(interval);
     }
   }
+  $(document).on('click', '.paginate_button, .btnGo', function() {
+    clearInterval(interval);
+    interval = setInterval(doStuff, 2000);
+  });
 });
